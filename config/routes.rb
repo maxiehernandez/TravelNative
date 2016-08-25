c83d4c1d369f0e6411f52d6c36171f6479316ee1
Maxie Hernandez <maxinehernandez@hotmail.com>
2016-08-25 17:50:51 -0400

    Generated sessions controller. Added routes for login/log out.

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,13 @@ Rails.application.routes.draw do
   resources :users
 
   root 'page#index'
-  
+
   get '/secret' => 'page#secret'
   get '/register' => 'users#new'
+
+# these routes will be for showing a login form, logging in, and logging out
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
 end
